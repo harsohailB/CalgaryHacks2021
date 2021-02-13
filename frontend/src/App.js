81c105d2ff004b5f7c01de5d8eef078ccc8f9926
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2021-02-13 15:01:28 -0700

    add material-ui theming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.backgroundColor,
+    fontSize: '16px'
+  }
+}))
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.root}>
       <h1>Frontend is alive :)</h1>
     </div>
   );
---
+++ frontend/src/theme/index.js
@@ -0,0 +1,8 @@
+const theme = {
+  backgroundColor: '#ffffff',
+  p: {
+    fontFamily: "'Sora', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;"
+  }
+}
+
+export default theme;
