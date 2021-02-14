76f49e6576351cbf4c5a6036f1e283a9ae527b0d
harsohailB <harsohail.brar1@gmail.com>
2021-02-14 09:20:15 -0700

    merge mastrer into 74

--- backend/models/Errand.js
+++ backend/models/Errand.js
@@ -13,14 +13,32 @@ const errand = new Schema({
   quester: { type: Schema.Types.ObjectId, ref: "users", autopopulate: "true" },
   review: { type: Schema.Types.ObjectId, ref: "reviews", autopopulate: "true" },
   tags: [{ type: Schema.Types.ObjectId, ref: "tags", autopopulate: "true" }],
-  messageThread: { type: Schema.Types.ObjectId, ref: "messagethreads", autopopulate: "true" },
-  status: { type: String, enum: ['AVAILABLE', 'ACCEPTED', 'IN_PROGRESS', 'COMPLETED'] },
-  type: { type: Schema.Types.ObjectId, ref: "errandTypes", autopopulate: "true" },
+  messageThread: {
+    type: Schema.Types.ObjectId,
+    ref: "messagethreads",
+    autopopulate: "true",
+  },
+  status: {
+    type: String,
+    enum: ["AVAILABLE", "ACCEPTED", "IN_PROGRESS", "COMPLETED"],
+  },
+  type: {
+    type: Schema.Types.ObjectId,
+    ref: "errandTypes",
+    autopopulate: "true",
+  },
   currentStageIdx: { type: Number, default: 0 },
-  applications: [{ type: Schema.Types.ObjectId, ref: "applications", autopopulate: "true" }],
-  address: [{ type: Schema.Types.ObjectId, ref: "addresses", autopopulate: "true "}],
+  applications: [
+    { type: Schema.Types.ObjectId, ref: "applications", autopopulate: "true" },
+  ],
+  address: {
+    type: Schema.Types.ObjectId,
+    ref: "addresses",
+    autopopulate: "true ",
+    default: null,
+  },
 });
 
-errand.plugin(require('mongoose-autopopulate'));
+errand.plugin(require("mongoose-autopopulate"));
 
 mongoose.model("errands", errand);
