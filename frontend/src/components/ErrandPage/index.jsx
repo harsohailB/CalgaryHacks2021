b87a7bb18c8dbd8ccec4ff48bd1f07402a506b5b
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2021-02-14 09:03:19 -0700

    remove sample code

--- frontend/src/components/ErrandPage/index.jsx
+++ frontend/src/components/ErrandPage/index.jsx
@@ -7,21 +7,6 @@ import UpdatesCard from "./UpdatesCard";
 import Message from "./MessagesCardWithInput";
 import useErrand from "./useErrand";
 
-const sampleErrand = {
-  name: "Gift Delivery",
-  price: 15,
-  startDateTime: null,
-  endDateTime: new Date(),
-  distance: 2.65,
-  description:
-    "Sample description here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat... ",
-  tags: ["Delivery", "Urgent"],
-  poster: {
-    name: "Poster Poster",
-  },
-  address: "2839 University Drive",
-};
-
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
