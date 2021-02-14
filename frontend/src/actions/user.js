7bc1d853f0c371ae2d44ba873b1e70c2d79b2bac
harsohailB <harsohail.brar1@gmail.com>
2021-02-13 23:06:06 -0700

    connect user api requests to forms

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -1,31 +1,30 @@
 import axios from "axios";
 import config from "../config";
+import { LOGIN } from "../contexts/types";
 
-export const addUser = async (username, email, password) => {
-  const body = { username, email, password };
+export const addUser = async (name, age, description, username, password) => {
+  const body = { name, age, description, username, password };
 
   // register user to database
-  const response = await axios.post(config.endpoint + "users", { ...body });
+  const response = await axios.post("/api/user", { ...body });
 
   if (response.status !== 200) {
     throw "Registration failed with error code " + response.status;
   }
 
   // update store with user info if successfully registered
-  return { user: response.data, type: LOGIN_USER };
+  return { type: LOGIN, payload: response.data };
 };
 
 export const loginUser = async (username, password) => {
   const body = { username, password };
 
   // register user to database
-  const response = await axios.get(config.endpoint + "auth", {
-    params: body,
-  });
+  const response = await axios.get("/api/auth", { params: body });
 
   if (response.status !== 200) {
     throw "Login failed with error code " + response.status;
   }
 
-  return { user: response.data, type: LOGIN_USER };
+  return { type: LOGIN, payload: response.data };
 };
