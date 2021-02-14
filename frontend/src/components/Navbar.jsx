0838b64b2080ff23da9c868c08881765294dd4e4
harsohailB <harsohail.brar1@gmail.com>
2021-02-13 20:21:10 -0700

    update navbar with pseudo user

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-import { AppBar, Grid, Toolbar, Typography, Button } from "@material-ui/core";
+import {
+  AppBar,
+  Grid,
+  Toolbar,
+  Typography,
+  Button,
+  Avatar,
+  Select,
+  MenuItem,
+  Tooltip,
+} from "@material-ui/core";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import Link from "@material-ui/core/Link";
-import React from "react";
+import React, { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -18,6 +29,16 @@ const useStyles = makeStyles((theme) =>
 
 const Navbar = () => {
   const classes = useStyles();
+  const [role, setRole] = useState("");
+
+  // Temporary User
+  const user = {
+    id: "temp",
+    name: "Harsohail Brar",
+    age: 21,
+    rating: 8.2,
+    description: "Software Engineering Student",
+  };
 
   return (
     <AppBar position="static" elevation={0} className={classes.root}>
@@ -36,38 +57,91 @@ const Navbar = () => {
             </Typography>
           </Grid>
 
-          <Grid
-            container
-            item
-            xs
-            justify="flex-end"
-            alignItems="center"
-            spacing={4}
-          >
-            <Grid item>
-              <Button
-                href="/signup"
-                variant="outlined"
-                size="medium"
-                color="primary"
-                className={classes.button}
-              >
-                Sign Up
-              </Button>
-            </Grid>
+          {user ? (
+            <Grid
+              container
+              item
+              xs
+              justify="flex-end"
+              alignItems="center"
+              spacing={4}
+            >
+              <Grid item>
+                <Button
+                  href="/quester"
+                  variant="outlined"
+                  size="medium"
+                  color="primary"
+                  className={classes.button}
+                  disabled={role === "quester"}
+                  onClick={() => setRole("quester")}
+                >
+                  Quester
+                </Button>
+              </Grid>
 
-            <Grid item>
-              <Button
-                href="/login"
-                variant="contained"
-                size="medium"
-                color="primary"
-                className={classes.button}
-              >
-                Login
-              </Button>
+              <Grid item>
+                <Button
+                  href="/poster"
+                  variant="outlined"
+                  size="medium"
+                  color="primary"
+                  className={classes.button}
+                  onClick={() => setRole("poster")}
+                >
+                  Poster
+                </Button>
+              </Grid>
+
+              <Grid item>
+                <Link
+                  href={`/profile/${user.name}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Tooltip placement="bottom" title="Edit Profile">
+                    <Avatar alt={user.name} src="placeholder" />
+                  </Tooltip>
+                </Link>
+              </Grid>
+
+              <Typography variant="p" color="textPrimary">
+                {user.name}
+              </Typography>
             </Grid>
-          </Grid>
+          ) : (
+            <Grid
+              container
+              item
+              xs
+              justify="flex-end"
+              alignItems="center"
+              spacing={4}
+            >
+              <Grid item>
+                <Button
+                  href="/signup"
+                  variant="outlined"
+                  size="medium"
+                  color="primary"
+                  className={classes.button}
+                >
+                  Sign Up
+                </Button>
+              </Grid>
+
+              <Grid item>
+                <Button
+                  href="/login"
+                  variant="contained"
+                  size="medium"
+                  color="primary"
+                  className={classes.button}
+                >
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+          )}
         </Grid>
       </Toolbar>
     </AppBar>
--- frontend/src/components/SignUp/index.jsx
+++ frontend/src/components/SignUp/index.jsx
@@ -1,110 +1,126 @@
-import { Box, Button, Container, Grid, Link, TextField, Typography} from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(2),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-  }));
+  paper: {
+    marginTop: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
 
-const SignUp= () => {
-    const classes = useStyles();
+const SignUp = () => {
+  const classes = useStyles();
 
-    return (
-        <Container component="main" maxWidth="sm" marginTop="20%">
-            <Box m={5} pt={2}></Box>
-            <center>
-            <Typography variant="h3" color="textPrimary">
+  return (
+    <Container component="main" maxWidth="sm" marginTop="20%">
+      <Box m={5} pt={2}></Box>
+      <center>
+        <Typography variant="h3" color="textPrimary">
+          Sign Up
+        </Typography>
+      </center>
+      <div className={classes.paper}>
+        <form className={classes.form} noValidate paddingTop={10}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="age"
+                label="Age"
+                type="number"
+                id="age"
+                autoComplete="current-age"
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="description"
+                label="Description"
+                type="text"
+                id="description"
+                autoComplete="current-description"
+              />
+            </Grid>
+          </Grid>
+
+          <Box m={1} pt={2}></Box>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
             Sign Up
-            </Typography>
-            </center>
-            <div className={classes.paper}>
-                <form className={classes.form} noValidate paddingTop={10}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                    <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="username"
-                        label="Username"
-                        name="username"
-                        autoComplete="username"
-                        autoFocus
-                    />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                    <TextField
-                        autoComplete="fname"
-                        name="firstName"
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="firstName"
-                        label="First Name"
-                    />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                    <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="lastName"
-                        label="Last Name"
-                        name="lastName"
-                        autoComplete="lname"
-                    />
-                    </Grid>
-                    <Grid item xs={12}>
-                    <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                    />
-                    </Grid>
-                    <Grid item xs={12}>
-                    <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    </Grid>
-                </Grid>
-                <Box m={1} pt={2}></Box>
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                >
-                    Sign Up
-                </Button>
-                <Box m={1} pt={1}></Box>
-                <Grid container justify="flex-end">
-                    <Grid item>
-                    <Link href="/login" variant="body1">
-                        Already signed up? Sign in here
-                    </Link>
-                    </Grid>
-                </Grid>
-                </form>
-            </div>
-        </Container>
-    );
-}
+          </Button>
+          <Box m={1} pt={1}></Box>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body1">
+                Already signed up? Sign in here
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+};
 
 export default SignUp;
